37fa9a8c99ef1e391d9ed93f9a62b11202c1f743
mima <mihalomatijevic@gmail.com>
2017-06-15 15:42:12 +0200

    reduce code in WordCount using Java 8.

--- sparkjobs/src/main/java/com/zuehlke/shmack/sparkjobs/wordcount/WordCount.java
+++ sparkjobs/src/main/java/com/zuehlke/shmack/sparkjobs/wordcount/WordCount.java
@@ -1,65 +1,46 @@
 package com.zuehlke.shmack.sparkjobs.wordcount;
 
-import java.io.Serializable;
-import java.util.Arrays;
-
+import com.zuehlke.shmack.sparkjobs.base.SortedCounts;
+import com.zuehlke.shmack.sparkjobs.base.TestableSparkJob;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
-
-import com.zuehlke.shmack.sparkjobs.base.SortedCounts;
-import com.zuehlke.shmack.sparkjobs.base.TestableSparkJob;
-
 import scala.Tuple2;
 import twitter4j.Status;
-import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
+
+import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * @see http://spark.apache.org/examples.html
  */
 public class WordCount extends TestableSparkJob<SortedCounts<String>> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private final String inputFile;
+    private static final long serialVersionUID = 1L;
+    private final String inputFile;
 
-	public WordCount(final String inputFile) {
-		this.inputFile = inputFile;
-	}
+    public WordCount(final String inputFile) {
+        this.inputFile = inputFile;
+    }
 
-	@SuppressWarnings("serial")
-	@Override
-	public SortedCounts<String> execute(final JavaSparkContext spark) {
-		final JavaRDD<String> textFile = spark.textFile(inputFile);
-		final JavaRDD<String> words = textFile.flatMap(new FlatMapFunction<String, String>() {
-			@Override
-			public Iterable<String> call(final String rawJSON) throws TwitterException {
-				final Status tweet = TwitterObjectFactory.createStatus(rawJSON);
-				String text = tweet.getText();
-				return Arrays.asList(text.split(" "));
-			}
-		});
-		final JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
-			@Override
-			public Tuple2<String, Integer> call(final String s) {
-				return new Tuple2<String, Integer>(s.toLowerCase(), 1);
-			}
-		});
-		final JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-			@Override
-			public Integer call(final Integer a, final Integer b) {
-				return a + b;
-			}
-		});
-		return SortedCounts.create(counts);
-	}
+    @SuppressWarnings("serial")
+    @Override
+    public SortedCounts<String> execute(final JavaSparkContext spark) {
+        final JavaRDD<String> textFile = spark.textFile(inputFile);
+        final JavaRDD<String> words = textFile.flatMap(s -> {
+            final Status tweet = TwitterObjectFactory.createStatus(s);
+            String text = tweet.getText();
+            return Arrays.asList(text.split(" "));
+        });
+        final JavaPairRDD<String, Integer> pairs = words.mapToPair( s -> new Tuple2<>(s.toLowerCase(), 1));
+        final JavaPairRDD<String, Integer> counts = pairs.reduceByKey((a, b) -> a + b);
+        return SortedCounts.create(counts);
+    }
 
-	@Override
-	public String getApplicationName() {
-		return "WordCount";
-	}
+    @Override
+    public String getApplicationName() {
+        return "WordCount";
+    }
 
 }
